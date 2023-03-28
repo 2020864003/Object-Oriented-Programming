6e25ad883cd2629c03f96439e88557c06342cb52
2020864003 <128778304+2020864003@users.noreply.github.com>
2023-03-28 17:06:25 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPROEJCT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack a = new Stack();
+
+            a.Push(1);
+            a.Push(2);
+            a.Push(3);
+
+            while (a.GetStackPointer() != -1)
+            {
+                Console.WriteLine(a.Pop());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Stack
+    {
+        private int[] stack;
+        private int sp = -1;
+        public Stack()
+        {
+            stack = new int[100];
+        }
+        public Stack(int size)
+        {
+            stack = new int[size];
+        }
+
+        public void Push(int data)
+        {
+            stack[++sp] = data;
+        }
+
+        public int Pop()
+        {
+            return stack[sp--];
+        }
+
+        public int GetStackPointer()
+        {
+            return sp;
+        }
+    }
+}
