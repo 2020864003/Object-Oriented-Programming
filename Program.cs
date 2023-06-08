5ea0125f7b17ee24001b01f7524ccc7272785dae
2020864003 <128778304+2020864003@users.noreply.github.com>
2023-06-08 16:23:37 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace OOPPROEJCT
+namespace firstProjectCSharp
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Stack a = new Stack();
-
-            a.Push(1);
-            a.Push(2);
-            a.Push(3);
-
-            while (a.GetStackPointer() != -1)
-            {
-                Console.WriteLine(a.Pop());
-            }
-
-            Console.ReadLine();
-        }
-    }
-
-    class Stack
-    {
-        private int[] stack;
-        private int sp = -1;
-        public Stack()
-        {
-            stack = new int[100];
-        }
-        public Stack(int size)
-        {
-            stack = new int[size];
-        }
-
-        public void Push(int data)
-        {
-            stack[++sp] = data;
-        }
-
-        public int Pop()
-        {
-            return stack[sp--];
-        }
-
-        public int GetStackPointer()
-        {
-            return sp;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form4());
         }
     }
 }
